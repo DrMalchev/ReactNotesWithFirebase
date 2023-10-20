4f044c512d0349e77b2d669f5a695bc2c0a182f4
DrMalchev <dr.delyan.malchev@gmail.com>
2023-10-20 11:58:47 +0300

    Debouncing requests to firebase.

--- App.jsx
+++ App.jsx
@@ -9,7 +9,8 @@ import { notesCollection, db } from "./firebase"
 export default function App() {
     const [notes, setNotes] = React.useState([])
     const [currentNoteId, setCurrentNoteId] = React.useState("")
-    
+    const [tempNoteText, setTempNoteText] = React.useState("")
+
     const currentNote = 
         notes.find(note => note.id === currentNoteId) 
         || notes[0]
@@ -36,6 +37,24 @@ export default function App() {
             setCurrentNoteId(notes[0]?.id)
         }
     }, [notes])
+
+    React.useEffect(() => {
+        if(currentNote)
+        {
+            setTempNoteText(currentNote.body)
+        }
+    }, [currentNote])
+
+    React.useEffect(() => {
+        const timeOutId = setTimeout(() => {
+            if(tempNoteText !== currentNote.body)
+            {
+                updateNote(tempNoteText)
+            }
+            
+        }, 500);
+        return () => clearTimeout(timeOutId)
+    },[tempNoteText])
 
     async function createNewNote() {
         const newNote = {            
@@ -78,8 +97,8 @@ export default function App() {
                             deleteNote={deleteNote}
                         />                                                    
                         <Editor
-                            currentNote={currentNote}
-                            updateNote={updateNote}
+                            tempNoteText={tempNoteText}
+                            setTempNoteText={setTempNoteText}
                         />                        
                     </Split>
                     :
